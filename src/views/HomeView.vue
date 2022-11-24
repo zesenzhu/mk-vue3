ce062806851926cf6dc14057f8206a5bc10ac762
祝泽森 <zhuzesen@zhouxiannv.local>
2022-11-24 22:35:56 +0800

    init

---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useCounterStore } from "../stores/counter";
+const counter = useCounterStore();
+const { count } = counter;
+console.log(counter, count);
+</script>
+
+<template>
+  <main>
+    <p>{{ counter.count }}</p>
+    <button @click="counter.increment">点击</button>
+  </main>
+</template>
